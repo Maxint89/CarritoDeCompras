fefefffbbde94c1ab8b411da2d0f7fd6c2a355e5
Maxint89 <maxwell_bj@hotmail.com>
2024-03-17 20:58:35 -0300

    Agregado de arrays, objetos y logica para segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,70 +1,125 @@
-function carritoDeCompras() {
+const prendas = [
+  { nombre: "remera", precio: 1000 },
+  { nombre: "buzo", precio: 2000 },
+  { nombre: "campera", precio: 3000 },
+  { nombre: "pantalon", precio: 4000 },
+  { nombre: "zapatillas", precio: 5000 }
+];
+
+let carrito = [];
+
+function saludarCliente() {
+  let nombreCliente;
+  let nombreValido = false;
+
+  do {
+      nombreCliente = prompt("Por favor, ingrese su nombre");
+      if (nombreCliente === null) {
+          alert("Por favor, ingrese su nombre para continuar.");
+      } else {
+          if (nombreCliente.length < 3 || !isNaN(nombreCliente)) {
+              alert("Por favor, ingrese un nombre válido.");
+          } else {
+              alert(`Bienvenido/a a Indumentaria del Pacífico ${nombreCliente}, ¿en qué podemos ayudarlo?`);
+              nombreValido = true;
+          }
+      }
+  } while (!nombreValido);
+}
+
+function comprarPrenda(opcion, cantidad) {
+  const prenda = prendas[opcion - 1];
+  const indicePrendaEnCarrito = carrito.findIndex(item => item.nombre === prenda.nombre);
+  if (indicePrendaEnCarrito !== -1) {
+      carrito[indicePrendaEnCarrito].cantidad += cantidad;
+      carrito[indicePrendaEnCarrito].total += prenda.precio * cantidad;
+      alert(`Usted ha agregado ${cantidad} ${prenda.nombre}(s) a su carrito. Su monto en ${prenda.nombre}(s) es de $${carrito[indicePrendaEnCarrito].total}`);
+  } else {
+      const totalPorPrenda = prenda.precio * cantidad;
+      carrito.push({ nombre: prenda.nombre, cantidad: cantidad, total: totalPorPrenda });
+      alert(`Usted a agregado ${cantidad} ${prenda.nombre}(s) a su carrito. Su monto en ${prenda.nombre}(s) es de $${totalPorPrenda}`);
+  }
+}
+
+function eliminarPrenda(nombre, totalGastado) {
+  const indicePrendaEnCarrito = carrito.findIndex(ropa => ropa.nombre === nombre);
+  if (indicePrendaEnCarrito !== -1) {
+      totalGastado = totalGastado - carrito[indicePrendaEnCarrito].total;
+      carrito.splice(indicePrendaEnCarrito, 1);
+      alert(`Se ha(n) quitado del carrito: ${nombre}(s)`);
+  } else {
+      alert(`No se encuentra(n) ${nombre}(s) en su carrito`);
+  }
+}
+
+function mostrarCarrito() {
+  let prendasEnElCarrito = "Usted a seleccionado:\n";
+  carrito.forEach(ropa => {
+      prendasEnElCarrito += `${ropa.cantidad} ${ropa.nombre}(s) - $${ropa.total}\n`;
+  });
+  alert(prendasEnElCarrito);
+}
+
+function calcularTotalGastado() { 
+  let total = 0;
+  for (let item of carrito) {
+      total += item.total;
+  }
+  return total;
+}
+
+function gestionarCompra() {
   let totalGastado = 0;
   let opcion;
   let cantidad = 0;
-  const remera = 6000;
-  const buzo = 12000;
-  const campera = 25000;
-  const pantalon = 18000;
-  const zapatillas = 50000;
+  let seguirComprando = true;
 
-  
-  alert("Bienvenido a Indumentaria del pacifico, en que podemos ayudarlo?");
+  saludarCliente();
 
   do {
-    opcion = prompt("Ingrese el número de la opción que desea comprar: \n1. Remera - $6000 \n2. Buzo - $12000 \n3. Campera - $25000 \n4. Pantalon - $18000 \n5. Zapatillas - $50000 \n0. Salir");
+      opcion = prompt("Ingrese el número de la opción que desea comprar: \n1. Remera - $1000 \n2. Buzo - $2000 \n3. Campera - $3000 \n4. Pantalon - $4000\n5. Zapatillas - $5000\n0. Salir/Eliminar del carrito");
 
-    if (isNaN(opcion)) {
-      alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 5 para comprar y 0 para salir");
-      continue;
-    } else {
-      opcion = parseInt(opcion);
-      if ((opcion < 0) || (opcion > 5)) {
-        alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 5 para comprar y 0 para salir");
-        continue;
+      if (isNaN(opcion)) {
+          alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 5 para comprar o 0 para salir o eliminar un producto del carrito");
+          continue;
+      } else {
+          opcion = parseInt(opcion);
+          if ((opcion < 0) || (opcion > 5)) {
+              alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 5 para comprar o 0 para salir o eliminar un producto del carrito");
+              continue;
+          }
       }
-    }
 
-    if (opcion !== 0) {
-      do {
-        cantidad = parseInt(prompt("Ingrese la cantidad de prendas que desea comprar:"));
-        if (isNaN(cantidad) || cantidad < 0) {
-          alert("Por favor, ingrese un número válido y mayor o igual a cero.");
-        }
-      } while (isNaN(cantidad) || cantidad < 0)
-    }
+      if (opcion !== 0) {
+          do {
+              cantidad = parseInt(prompt("Ingrese la cantidad de prendas que desea comprar:"));
+              if (isNaN(cantidad) || cantidad < 0) {
+                  alert("Por favor, ingrese un número válido y mayor o igual a cero.");
+              }
+          } while (isNaN(cantidad) || cantidad < 0)
+      }
 
-    switch (opcion) {
-      case 1:
-        totalGastado += remera * cantidad;
-        alert("Ha comprado " + cantidad + " remeras por $" + remera * cantidad);
-        break;
-      case 2:
-        totalGastado += buzo * cantidad;
-        alert("Ha comprado " + cantidad + " buzos por $" + buzo * cantidad);
-        break;
-      case 3:
-        totalGastado += campera * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + campera * cantidad);
-        break;
-      case 4:
-        totalGastado += pantalon * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + pantalon * cantidad);
-        break
-      case 5:
-        totalGastado += zapatillas * cantidad;
-        alert("Ha comprado " + cantidad + " camperas por $" + zapatillas * cantidad);
-        break
-      case 0:
-        alert("Saliendo...");
-        break;
-      default:
-        alert("Opción inválida. Por favor, seleccione nuevamente.");
-    }
-  } while (opcion !== 0);
+      if (opcion > 0) {
+          comprarPrenda(opcion, cantidad);
+      } else if (opcion === 0) {
+          const continuarComprandoRespuesta = prompt("Ingrese 'salir' para terminar la compra o 'eliminar' para quitar un producto del carrito").toLowerCase();
+          if (continuarComprandoRespuesta === "eliminar") {
+              const prendaAEliminar = prompt("Ingrese el nombre del tipo de prenda que desea quitar del carrito").toLowerCase();
+              eliminarPrenda(prendaAEliminar, totalGastado);
+          } else if (continuarComprandoRespuesta === "salir") {
+              seguirComprando = false;
+          } else {
+              alert("Seleccione una opcion valida. 'eliminar' para quitar un tipo de prenda del carrito o 'salir' para concluir la compra");
+          }
+      } else {
+          alert("Opcion invalida. Por favor seleccione una opcion entre 1 y 5 para comprar o 0 para salir o eliminar un producto del carrito");
+      }
+  } while (seguirComprando);
 
-  alert("El total gastado es de $" + totalGastado + ".");
+  mostrarCarrito();
+  totalGastado = calcularTotalGastado(); 
+  alert(`Debe abonar $${totalGastado} por su compra.`);
   alert("Gracias por elegirnos. Vuelva pronto.");
 }
 
-carritoDeCompras();
+gestionarCompra();
